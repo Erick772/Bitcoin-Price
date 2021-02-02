3400330dc6c62c9e2358dcfb2303cc71145ddb69
Erick772 <77893699+Erick772@users.noreply.github.com>
2021-02-02 10:28:18 -0300

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -8,8 +8,6 @@ from sys import argv
 
 moeda = argv[1]
 
-r = requests.get('https://blockchain.info/ticker')
-
-info = json.loads(r.text)
+info = json.loads(requests.get('https://blockchain.info/ticker').text)
 
 print('1 BTC = %s %.2f %s' % (info[moeda]['symbol'], info[moeda]['buy'], moeda))
